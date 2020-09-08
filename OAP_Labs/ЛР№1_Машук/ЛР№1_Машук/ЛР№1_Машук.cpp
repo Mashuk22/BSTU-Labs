6f0b1038e7b4499999f338ccd0114bdf088b41b8
Mashuk22 <i_mashuk@mail.ru>
2020-09-08 09:13:32 +0300

    commit1

---
+++ OAP_Labs/ЛР№1_Машук/ЛР№1_Машук/ЛР№1_Машук.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+    int x, y, z, k;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+
+    if (x + y + z / 2 < x * y * z)
+        k = x + y + z / 2;
+    else
+        k = x * y * z;
+    
+    if (x * y > y)
+        cout << k + x * y;
+    else
+        cout << k + y;
+
+}
